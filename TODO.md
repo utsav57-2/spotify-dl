88b2cfda090cce87a44700d54501b8e2c2b6ed07
piyushrungta25 <piyushrungta25@gmail.com>
2016-06-18 02:00:12 +0530

    youtube links are fetched added todo.md

---
+++ TODO.md
@@ -0,0 +1,3 @@
+## TODO
+
+- Better youtube search by matching yt channel and looking for words like 'official' or 'original'
--- listing.py
+++ listing.py
@@ -3,8 +3,10 @@ import urllib2
 import urllib
 import json
 import sys
+from urllib import quote_plus as qp
 
 ALBUM_ART = False
+DEBUG = False
 
 def get_playlist_json(album_url):
     result = urllib2.urlopen(album_url)
@@ -38,10 +40,9 @@ def get_playlist(album_url):
     playlist = []
     for song in lst:
         album=song["track"]["album"]["name"]
-        artists=""
-        for artist in song["track"]["artists"]:
-            artists = artists + artist["name"] + ","
-        artists = artists[:-1]
+        artists=[artist["name"] for artist in song["track"]["artists"]]
+        # for artist in song["track"]["artists"]:
+        #     artists.append(artist["name"])
 
         # Duration info
         #
@@ -56,15 +57,47 @@ def get_playlist(album_url):
         #     duration = str(mins) + ":" + str(secs)
 
         name = song["track"]["name"]
-        image_name = artists + " - " + album + ".jpg"
+        # image_name = artists + " - " + album + ".jpg"
         # urllib.urlretrieve(song["track"]["album"]["images"][0]["url"],image_name.encode("utf-8"))
         # out_str = artists + " - " + name + " - " + album + " - " + str(duration)
-        playlist.append( [ artists,name,album ] )
+        playlist.append( { "artists":artists,"song_name":name,"album":album } )
 
     return playlist
 
 def get_youtube_links(pl):
-    pass
+    base_url = "https://www.youtube.com/results?search_query="
+    yt_links = []
+
+    for i in pl:
+        query = '' + qp(i["song_name"])
+
+        artist_arr = [qp(j) for j in i["artists"]]
+        artists = artist_arr[0]
+        for j in artist_arr[1:]:
+            artists = artists + '+' + j
+
+        query += '+'+artists
+        req_url = base_url + query
+
+        if DEBUG:
+            for i in req_url:
+                print i
+
+        result = urllib2.urlopen(req_url)
+        html = result.read()
+        soup = BeautifulSoup(html,"lxml")
+
+        links = soup.find_all('h3',class_='yt-lockup-title')
+        # TODO
+        links_texts = [i.a.string for i in links]
+        links_arr = [i.a['href'] for i in links]
+        yt_links.append(links_arr[0])
+
+    return yt_links
+
+
+
+
 
 def main():
     album_url = sys.argv[1]
@@ -76,6 +109,9 @@ def main():
 
     yt_links = get_youtube_links(pl)
 
+    if DEBUG:
+        for i in yt_links:
+            print 'https://www.youtube.com'+i
 
 
 
